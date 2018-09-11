02371c13be181b7cc3c525b2ce2bf29705c771d1
Hghhhh <1025233032@qq.com>
2018-09-11 23:29:31 +0800

    测试文件

---
+++ testFile/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ *测试文件
+ *
+ */
+class HelloWorld {
+	 public static void main(String[] args){
+		System.out.println("HelloWorld");
+		//打印内容
+	}
+
+}
---
+++ testFile/OneLine.java
@@ -0,0 +1,1 @@
+just one line in the file for test
